928d0dc1345718676014f83fbbb7e948b5205699
Plamen Kinev <plamenkinev@gmail.com>
2014-03-10 11:01:08 +0200

    Changed password field to be of type password on register.jsp

--- ProductMarketWeb/src/market/login/ProductService.java
+++ ProductMarketWeb/src/market/login/ProductService.java
@@ -4,19 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProductService {
+	
 	private List<Product> products = new ArrayList<Product>();
 	private static ProductService instance = null;
+	
 	private ProductService () {
 		this.products.add(new Product(12, "Bag","Small light one-hand bag", 5.99));
 		this.products.add(new Product(111, "Hat","Red velvet barette", 15.34));
-
 	}
+	
 	public static ProductService getInstance () {
 		if(instance==null) {
 			instance = new ProductService();
 		}
 		return instance;
 	}
+	
 	public List<Product> getProducts(){
 		return products;
 	}
